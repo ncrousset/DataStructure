23b1ddb7a5bb617262f1b3f2fd33d1593deb9b52
Rudys Natanael Acosta Crousset <natanael926@gmail.com>
2020-08-27 16:37:20 -0400

    start project

---
+++ src/LinkedList/LinkedList.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\LinkedList;
+
+class LinkedList
+{
+
+    public function __construct()
+    {
+        echo "Hola";
+    }
+}
---
+++ tests/LinkedList/LinkedListTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\LinkedList\LinkedList;
+use PHPUnit\Framework\TestCase;
+
+class LinkedListTest extends TestCase
+{
+    public function testOk()
+    {
+        $instace = new LinkedList();
+
+
+        $this->assertTrue(true);
+    }
+}
